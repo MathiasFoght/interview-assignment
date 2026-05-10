9d90f60657b06edbf2e953ad0c57fbce1af97057
Mathias Foght <mathiasfoght98@gmail.com>
2026-05-10 12:54:10 +0200

    configure project deps and tooling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Enable caching for server components
+  cacheComponents: true,
 };
 
 export default nextConfig;
